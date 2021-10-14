abe176893d3c8d2b158e1c5fd3b83f5c851d968b
AlexiaAM <alexia77755@gmail.com>
2021-10-13 22:02:47 -0500

    Login interfaz creada

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -8,7 +8,7 @@ export class Layout extends Component {
   render () {
     return (
       <div>
-        <NavMenu userType={2}/>
+        <NavMenu userType={0}/>
         <Container>
           {this.props.children}
         </Container>
--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,11 +1,48 @@
 import React from "react";
 
 class Login extends React.Component {
+
+    state = {
+        usuario : '',
+        clave : ''
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
+
     render(){
         return (
-            <div>
-                Login
-            </div>
+            <div> 
+            <form>
+                <div className="form-group" style={{margin: "5% 30% 5% 30%"}}>
+                    <label>Usuario: </label>
+                    <input 
+                        name = "usuario"
+                        className="form-control"
+                        onChange= { this.handleInputChange }
+                        value= {this.state.usuario}    
+                    />
+                    <label>Clave: </label>
+                    <input 
+                        name = "clave"
+                        className="form-control" 
+                        type="password"
+                        onChange= { this.handleInputChange }
+                        value= {this.state.clave}
+                    />
+                    
+                    
+                    <button className="btn btn-success" style={{marginTop: "10px"}}>Ingresar</button>
+                </div>
+            </form> 
+        </div> 
         );
     }
 }
