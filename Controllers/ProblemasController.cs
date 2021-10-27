b23fcf6666b58d711c00d1e74baad13d3ee35047
DT3264 <cernadaniel32@gmail.com>
2021-10-27 00:03:58 -0500

    Agregado CU15
    Permite agregar problemas, modificar problemas y obtener las categorias posibles de problemas

---
+++ Controllers/ProblemasController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQL_Judge.BD;
+using SQL_Judge.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQL_Judge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProblemasController : ControllerBase
+    {
+        /// <summary>
+        /// Inserta un nuevo problema
+        /// </summary>
+        /// /// Ejemplo:
+        ///
+        ///     POST /api/problemas/agregaProblema
+        ///     {
+        ///        "Nombre": "Las montañas más altas",
+        ///        "Descripcion": "En este problema debes seleccionar las 10 montañas más altas",
+        ///        "Solucion": "select * from motañas order by altura limit 10",
+        ///        "BaseDeDatos": "zonas",
+        ///        "Categoria": 1,
+        ///        "Dificultad": 500
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">El problema se agregó exitosamente</response>
+        [HttpPost("agregaProblema")]
+        [Authorize(Policy = "Admins")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public IActionResult AgregaProblema([FromBody]AgregarProblemaRequest request)
+        {
+            var dbContext = new SQLJudgeContext();
+            var nuevoProblema = new Problema()
+            {
+                Nombre = request.Nombre,
+                Descripcion = request.Descripcion,
+                Solucion = request.Solucion,
+                BaseDeDatos = request.BaseDeDatos,
+                Categoria = request.Categoria,
+                Dificultad = request.Dificultad
+            };
+            dbContext.Add(nuevoProblema);
+            dbContext.SaveChanges();
+            return Ok("Problema agregado correctamente");
+        }
+
+        /// <summary>
+        /// Inserta un nuevo problema
+        /// </summary>
+        /// /// Ejemplo:
+        ///
+        ///     POST /api/problemas/modificarProblema
+        ///     {
+        ///        "IdProblema": 1,
+        ///        "Nombre": "Las montañas más altas",
+        ///        "Descripcion": "En este problema debes seleccionar las 20 montañas más altas",
+        ///        "Solucion": "select * from motañas order by altura limit 20",
+        ///        "BaseDeDatos": "zonas",
+        ///        "Categoria": 1,
+        ///        "Dificultad": 500
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">El problema se modificó exitosamente</response>
+        [HttpPost("modificarProblema")]
+        [Authorize(Policy = "Admins")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public IActionResult ModificarProblema([FromBody] ModificarProblemaRequest request)
+        {
+            var dbContext = new SQLJudgeContext();
+            var problemaAActualizar = new Problema()
+            {
+                IdProblema = request.IdProblema,
+                Nombre = request.Nombre,
+                Descripcion = request.Descripcion,
+                Solucion = request.Solucion,
+                BaseDeDatos = request.BaseDeDatos,
+                Categoria = request.Categoria,
+                Dificultad = request.Dificultad
+            };
+            dbContext.Update(problemaAActualizar);
+            dbContext.SaveChanges();
+            return Ok("Problema modificado correctamente");
+        }
+    }
+}
---
+++ Responses/ObtenerCategoriasResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQL_Judge.Responses
+{
+    public class ObtenerCategoriasResponse
+    {
+        [DefaultValue(1)]
+        public int IdCategoria { get; set; }
+        [DefaultValue("Join")]
+        public string Nombre { get; set; }
+    }
+}
