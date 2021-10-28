8fcb915e9ff4da55a1476a17a75433d055d62c45
DT3264 <cernadaniel32@gmail.com>
2021-10-28 15:44:17 -0500

    Corregida autorizacion

--- Controllers/BasesController.cs
+++ Controllers/BasesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SQL_Judge.BD;
 using SQL_Judge.Responses;
@@ -9,15 +10,18 @@ using System.Threading.Tasks;
 
 namespace SQL_Judge.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BasesController : ControllerBase
     {
         /// <summary>
         /// Obbtiene las bases existentes
         /// </summary>
         /// <response code="200">Regresa las bases existentes</response>  
-        [HttpPost("obtenerBasess")]
+        [HttpPost("obtenerBases")]
+        [Authorize(Policy = "Admins")]
         [ProducesResponseType(typeof(List<ObtenerBasesResponse>), StatusCodes.Status200OK)]
         public IActionResult ObtenerCategorias()
         {
