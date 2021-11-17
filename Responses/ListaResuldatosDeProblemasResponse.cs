89b20f91cbde8bd5f4daaa85d54d8fc30843ac55
DT3264 <cernadaniel32@gmail.com>
2021-11-17 11:44:23 -0600

    Agregados metodos de envio
    Agregado
    listaResutladosDeProblemas
    listaProblemasPorID

---
+++ Responses/ListaResuldatosDeProblemasResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQL_Judge.Responses
+{
+    public class ListaResuldatosDeProblemasResponse
+    {
+        [DefaultValue(typeof(ResultadoResponse))]
+        List<ResultadoResponse> resultados { get; set; }
+    }
+    public class ResultadoResponse
+    {
+        [DefaultValue("Andros_")]
+        public string usuario { get; set; }
+        [DefaultValue("Luis Andres Gutierrez Calderon")]
+        public string nombreCompleto { get; set; }
+        [DefaultValue(typeof(List<string>))]
+        public List<string> veredictos { get; set; }
+
+    }
+}
