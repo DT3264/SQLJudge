cb516e9269f0ad8961b1dc9c69c9daa01c55c9de
DT3264 <cernadaniel32@gmail.com>
2022-09-05 12:00:14 -0500

    Remueve dummy text de la página de inicio

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Home extends Component {
   static displayName = Home.name;
 
-
-  componentDidMount(){
-    document.title = "SQL Judge"
+  componentDidMount() {
+    document.title = "SQL Judge";
   }
 
-  render () {
+  render() {
     return (
       <div>
         <h1>Bienvenido al SQL Judge</h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin maximus sapien eu gravida. 
-            Phasellus posuere, elit a blandit luctus, neque ante egestas turpis, eu malesuada mauris tortor quis enim. 
-            Maecenas quis nulla non ligula accumsan lacinia. Mauris venenatis tincidunt enim vel pulvinar.
-        </p>
       </div>
     );
   }
